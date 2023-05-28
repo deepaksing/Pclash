ce3fa7fabcdc3aed94befa9f6d85238b9ea7541f
deepaksing <deepak123s456@gmail.com>
2023-05-28 20:51:19 +0530

    added information to the player cards

--- web/app/game/page.jsx
+++ web/app/game/page.jsx
@@ -49,8 +49,13 @@ const Game = () => {
             <div className="card card1">
               <div className="card_inside">
                 <div className="card_inside_charachter"></div>
-                <div className="card_inside_charname"></div>
-                <div className="card_inside_description"></div>
+                <div className="card_inside_charname">Batty Guest</div>
+                <div className="card_inside_description">
+                  <span className="card_inside_description_title">
+                    Deathrattle:
+                  </span>
+                  summon a 2/1 Bat.
+                </div>
               </div>
 
               <div className="card_mana_required">
@@ -67,6 +72,17 @@ const Game = () => {
             </div>
 
             <div className="card card2">
+              <div className="card_inside">
+                <div className="card_inside_charachter"></div>
+                <div className="card_inside_charname">Batty Guest</div>
+                <div className="card_inside_description">
+                  <span className="card_inside_description_title">
+                    Deathrattle:
+                  </span>
+                  summon a 2/1 Bat.
+                </div>
+              </div>
+
               <div className="card_mana_required">
                 <div className="card_mana__req_text">3</div>
               </div>
@@ -81,20 +97,17 @@ const Game = () => {
             </div>
 
             <div className="card card3">
-              <div className="card_mana_required">
-                <div className="card_mana__req_text">3</div>
+              <div className="card_inside">
+                <div className="card_inside_charachter"></div>
+                <div className="card_inside_charname">Batty Guest</div>
+                <div className="card_inside_description">
+                  <span className="card_inside_description_title">
+                    Deathrattle:
+                  </span>
+                  summon a 2/1 Bat.
+                </div>
               </div>
 
-              <div className="card_attack_power">
-                <div className="attack_power_text">2</div>
-              </div>
-
-              <div className="card_health_power">
-                <div className="card_health_text">4</div>
-              </div>
-            </div>
-
-            <div className="card card4">
               <div className="card_mana_required">
                 <div className="card_mana__req_text">3</div>
               </div>
